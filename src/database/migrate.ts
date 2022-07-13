05045785232a1051796ece377c8f4a064466bdd7
Dan Schultz <slifty@gmail.com>
2022-07-13 13:33:33 -0400

    Separate database packages
    This is a very small refactor which aligns more with the spirit of our
    repo which is that meaningful exports are placed in their own file and
    consolidated by an index.ts.

---
+++ src/database/migrate.ts
@@ -0,0 +1,11 @@
+import path from 'path';
+import { migrate as pgMigrate } from 'postgres-migrations';
+import { db } from './db';
+
+export const migrate = async (): Promise<void> => {
+  const client = await db.getClient();
+  await pgMigrate(
+    { client },
+    path.resolve(__dirname, 'migrations'),
+  );
+};

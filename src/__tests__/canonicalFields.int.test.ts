b22fb2033f89d7bb132fd2528f1d0c41b37d1252
Dan Schultz <slifty@gmail.com>
2022-07-21 14:12:16 -0400

    Run database setup and teardown via suite
    Manually calling the DB setup / teardown within a test was verbose, but
    more importantly it meant teardown wouldn't happen if a test failed.
    That's no good!
    
    By using Jest's suite functionality we ensure that setup and teardown
    both run for each test regardless of test outcome.
    
    Issue #43 Support integration tests

--- src/__tests__/canonicalFields.int.test.ts
+++ src/__tests__/canonicalFields.int.test.ts
@@ -1,20 +1,14 @@
 import request from 'supertest';
 import { app } from '../app';
-import {
-  prepareDatabaseForCurrentWorker,
-  cleanupDatabaseForCurrentWorker,
-} from '../test/harnessFunctions';
 
 const agent = request.agent(app);
 
 describe('/canonicalFields', () => {
   describe('/', () => {
     it('should return HTTP Status Code 200 OK', async () => {
-      await prepareDatabaseForCurrentWorker();
       await agent
         .get('/canonicalFields')
         .expect(200);
-      await cleanupDatabaseForCurrentWorker();
     });
   });
 });

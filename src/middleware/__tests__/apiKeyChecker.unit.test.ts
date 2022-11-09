bf3d44d5757c8213efee707a69ebdb628ddce5e1
Ghulam Murtaza <gmurtaza@opentechstrategies.com>
2022-11-09 12:33:36 -0500

    Improved test cases according to ARRANGE-ACT-ASSERT pattern

--- src/middleware/__tests__/apiKeyChecker.unit.test.ts
+++ src/middleware/__tests__/apiKeyChecker.unit.test.ts
@@ -4,7 +4,6 @@ import { checkApiKey } from '../apiKeyChecker';
 import type { NextFunction, Request, Response } from 'express';
 
 describe('Authorization middleware', () => {
-  let mockRequest: Partial<Request> = {};
   const mockResponse: Partial<Response> = {};
   const nextFunction: NextFunction = jest.fn();
 
@@ -13,30 +12,36 @@ describe('Authorization middleware', () => {
   });
 
   it('without x-api-key header', async () => {
-    const expectedResponse = new AuthenticationError('API key not provided in the header "x-api-key"');
-    mockRequest = {
+    const mockRequest: Partial<Request> = {
       headers: {
         'x-api-key': '',
       },
     };
     checkApiKey(mockRequest as Request, mockResponse as Response, nextFunction);
-    expect(nextFunction).toBeCalledWith(expectedResponse);
+    expect(nextFunction).toBeCalledWith(
+      new AuthenticationError(
+        'API key not provided in the header "x-api-key"',
+      ),
+    );
   });
 
   it('with wrong api header', async () => {
-    const expectedResponse = new AuthenticationError('Invalid api key provided');
-    mockRequest = {
+    const mockRequest: Partial<Request> = {
       headers: {
         'x-api-key': 'philanthropydatacommons',
       },
     };
     checkApiKey(mockRequest as Request, mockResponse as Response, nextFunction);
-    expect(nextFunction).toBeCalledWith(expectedResponse);
+    expect(nextFunction).toBeCalledWith(
+      new AuthenticationError(
+        'Invalid api key provided',
+      ),
+    );
   });
 
   it('with correct api header', async () => {
     const data = fs.readFileSync('test_keys.txt', 'utf8').split('\n');
-    mockRequest = {
+    const mockRequest: Partial<Request> = {
       headers: {
         'x-api-key': data[0],
       },

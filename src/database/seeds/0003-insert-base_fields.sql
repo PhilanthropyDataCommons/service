4bdf1ffe88521e546971bb74b5e1d2bb14b74d1f
Jesse Bickel <jbickel@opentechstrategies.com>
2023-06-12 14:42:43 -0500

    Add data seed helper scripts 3 and 4
    Developers or users of the API can use this script to add base fields
    to their local databases. It is intended to run after the preceding
    scripts. In other words, even if you are just starting out, run the
    script starting with `0001` followed by `0002` and so forth.
    
    Issue #102 Make base fields and application forms from spreadsheets

---
+++ src/database/seeds/0003-insert-base_fields.sql
@@ -0,0 +1,8 @@
+INSERT INTO base_fields (label, short_code, data_type) VALUES
+('Proposal Primary Contact Organization', 'proposal_primary_contact_organization_name', 'freeFormText' ),
+('Proposal Primary Contact County', 'proposal_primary_contact_county', 'freeFormText' ),
+('Have you applied for RWJF funding in the past?', 'have_you_applied_for_rwjf_funding_in_the_past', 'trueOrFalse' ),
+('Have you applied for RWJF funding through the Pioneering Ideas funding opportunity in the past?', 'have_you_applied_for_rwjf_funding_through_the_pioneering_ideas_funding_opportunity_in_the_past', 'trueOrFalse' ),
+('Proposal Heard About Opportunity From', 'proposal_heard_about_opportunity_from', 'freeFormText' ),
+('Organization Unit', 'organization_unit', 'freeFormText' ),
+('Proposal Primary Contact Degree', 'proposal_primary_contact_degree', 'freeFormText' );
---
+++ src/database/seeds/0004-insert-base_fields.sql
@@ -0,0 +1,16 @@
+INSERT INTO base_fields (label, short_code, data_type) VALUES
+('Organization IRS Name', 'organization_irs_name', 'freeFormText' ),
+('Organization IRS Address', 'organization_irs_address', 'freeFormText' ),
+('Organization IRS City', 'organization_irs_city', 'freeFormText' ),
+('Organization IRS State', 'organization_irs_state', 'freeFormText' ),
+('Organization IRS Zip', 'organization_irs_zip', 'freeFormText' ),
+('Organization IRS Subsection', 'organization_irs_subsection', 'freeFormText' ),
+('Organization IRS Classification', 'organization_irs_classification', 'freeFormText' ),
+('Organization IRS Foundation Information', 'organization_irs_foundation_information', 'freeFormText' ),
+('Organization Tax Period', 'organization_tax_period', 'date' ),
+('Organization Ruling Date', 'organization_ruling_date', 'date' ),
+('Organization Demographic Info Required', 'organization_demographic_info_required', 'trueOrFalse' ),
+('Organization Demographics Last Updated', 'organization_demographics_last_updated', 'date' ),
+('Organization Audit Last Completed', 'organization_audit_last_completed', 'date' ),
+('Organization Audit Auditor Name', 'organization_audit_auditor_name', 'freeFormText' ),
+('Organization Audit Important Notes', 'organization_audit_important_notes', 'freeFormText');

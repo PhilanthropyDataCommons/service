d151a88dc8222c1a984a68753c976f379144d131
Jason Owen <jasonaowen@opentechstrategies.com>
2023-11-21 21:57:52 -0800

    Require S3 client environment variables

--- src/s3Client.ts
+++ src/s3Client.ts
@@ -1,11 +1,22 @@
 import { S3 } from '@aws-sdk/client-s3';
+import { requireEnv } from 'require-env-variable';
+
+const {
+  S3_ACCESS_KEY_ID,
+  S3_ACCESS_SECRET,
+  S3_ENDPOINT,
+} = requireEnv(
+  'S3_ACCESS_KEY_ID',
+  'S3_ACCESS_SECRET',
+  'S3_ENDPOINT',
+);
 
 export const s3Client = new S3({
   forcePathStyle: false,
-  endpoint: process.env.S3_ENDPOINT as string,
+  endpoint: S3_ENDPOINT,
   region: 'us-east-1', // This is required but not actually used by the S3 sdk
   credentials: {
-    accessKeyId: process.env.S3_ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.S3_ACCESS_SECRET as string,
+    accessKeyId: S3_ACCESS_KEY_ID,
+    secretAccessKey: S3_ACCESS_SECRET,
   },
 });

c3bcb1f583a51d8fafaf07fe3ed4a141c58a7e92
Jason Owen <jasonaowen@opentechstrategies.com>
2023-11-21 22:18:58 -0800

    Make S3 provider more configurable
    Different S3-compatible providers have different requirements, including
    the region name and whether to make path- or subdomain-style requests.
    DigitalOcean does not care about the region name, but S3 itself does;
    likewise, S3 and DigitalOcean want to use subdomain-style requests, but
    Garage and Minio tend to want to use path-style requests.
    
    Make the region name and request style configurable via environment
    variables.

--- src/s3Client.ts
+++ src/s3Client.ts
@@ -5,16 +5,20 @@ const {
   S3_ACCESS_KEY_ID,
   S3_ACCESS_SECRET,
   S3_ENDPOINT,
+  S3_PATH_STYLE,
+  S3_REGION,
 } = requireEnv(
   'S3_ACCESS_KEY_ID',
   'S3_ACCESS_SECRET',
   'S3_ENDPOINT',
+  'S3_PATH_STYLE',
+  'S3_REGION',
 );
 
 export const s3Client = new S3({
-  forcePathStyle: false,
+  forcePathStyle: S3_PATH_STYLE === 'true',
   endpoint: S3_ENDPOINT,
-  region: 'us-east-1', // This is required but not actually used by the S3 sdk
+  region: S3_REGION,
   credentials: {
     accessKeyId: S3_ACCESS_KEY_ID,
     secretAccessKey: S3_ACCESS_SECRET,

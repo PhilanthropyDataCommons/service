1e19efd0099531dbb5101cfbd96d4ccbab728944
Dan Schultz <slifty@gmail.com>
2023-11-29 11:56:48 -0500

    Move base field creation schema to type file
    We shifted our pattern for things like creation schemas so that they're
    defined alongside the "root" type definition.  This makes it easier to
    change all versions of a given type in one place.

--- src/types/BaseField.ts
+++ src/types/BaseField.ts
@@ -1,3 +1,6 @@
+import { ajv } from '../ajv';
+import type { JSONSchemaType } from 'ajv';
+
 export interface BaseField {
   id: number;
   label: string;
@@ -5,3 +8,30 @@ export interface BaseField {
   dataType: string;
   createdAt: Date;
 }
+
+// See https://github.com/typescript-eslint/typescript-eslint/issues/1824
+/* eslint-disable @typescript-eslint/indent */
+export type BaseFieldCreate = Omit<BaseField, 'createdAt' | 'id'>;
+/* eslint-enable @typescript-eslint/indent */
+
+export const baseFieldCreateSchema: JSONSchemaType<BaseFieldCreate> = {
+  type: 'object',
+  properties: {
+    label: {
+      type: 'string',
+    },
+    shortCode: {
+      type: 'string',
+    },
+    dataType: {
+      type: 'string',
+    },
+  },
+  required: [
+    'label',
+    'shortCode',
+    'dataType',
+  ],
+};
+
+export const isBaseFieldCreate = ajv.compile(baseFieldCreateSchema);

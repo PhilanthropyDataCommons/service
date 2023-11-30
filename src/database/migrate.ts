80bd3638003721a7f55b0a410a141ab1f4193fa3
Jason Owen <jasonaowen@opentechstrategies.com>
2023-11-29 16:10:03 -0800

    Log database migrations
    Our database migration manager supports logging progress by passing in a
    simple logging function. Call it with our logger so that we can see
    fine-grain migration progress detail.

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -1,7 +1,10 @@
 import path from 'path';
 import { migrate as pgMigrate } from 'postgres-schema-migrations';
 import { runJobQueueMigrations } from '../jobQueue';
+import { getLogger } from '../logger';
 import { db } from './db';
+
+const logger = getLogger(__filename);
 
 export const migrate = async (schema = 'public'): Promise<void> => {
   const client = await db.getClient();
@@ -9,7 +12,10 @@ export const migrate = async (schema = 'public'): Promise<void> => {
     await pgMigrate(
       { client },
       path.resolve(__dirname, 'migrations'),
-      { schema },
+      {
+        logger: (msg) => logger.info(msg),
+        schema,
+      },
     );
     await runJobQueueMigrations();
   } finally {

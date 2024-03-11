07f29e7758b52538e71129acfaf3aae559d96ae8
Dan Schultz <slifty@gmail.com>
2024-03-11 12:24:23 -0400

    Support specified timezone in ISO dates
    Postgres doesn't seem to be able to format ISO strings with `Z` even
    when in UTC (and instead uses `+00:00`).  This makes a change to support
    the ISO standard more accurately.
    
    I decided not to require 00:00 explicitly since the intention really is
    ISO, not UTC necessarily.

--- src/test/utils.ts
+++ src/test/utils.ts
@@ -1,5 +1,5 @@
 export const isoTimestampPattern =
-	/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
+	/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3,6}(Z|(\+|-)\d{2}:\d{2})$/;
 
 export const expectTimestamp = expect.stringMatching(
 	isoTimestampPattern,

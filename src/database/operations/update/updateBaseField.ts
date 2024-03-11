660da45fcef449ab1df6d160385fc8a70047e6aa
Dan Schultz <slifty@gmail.com>
2024-03-11 12:52:50 -0400

    Use json result set when updating BaseFields
    Issue #821 Change our query approach to use DB-level json functions

--- src/database/operations/update/updateBaseField.ts
+++ src/database/operations/update/updateBaseField.ts
@@ -1,20 +1,27 @@
 import { db } from '../../db';
 import { NotFoundError } from '../../../errors';
-import type { BaseField, WritableBaseField } from '../../../types';
+import type {
+	BaseField,
+	JsonResultSet,
+	WritableBaseField,
+} from '../../../types';
 
 export const updateBaseField = async (
 	id: number,
 	updateValues: WritableBaseField,
 ): Promise<BaseField> => {
 	const { label, description, shortCode, dataType } = updateValues;
-	const result = await db.sql<BaseField>('baseFields.updateById', {
-		id,
-		label,
-		description,
-		shortCode,
-		dataType,
-	});
-	const baseField = result.rows[0];
+	const result = await db.sql<JsonResultSet<BaseField>>(
+		'baseFields.updateById',
+		{
+			id,
+			label,
+			description,
+			shortCode,
+			dataType,
+		},
+	);
+	const baseField = result.rows[0]?.object;
 	if (baseField === undefined) {
 		throw new NotFoundError('This base field does not exist.');
 	}

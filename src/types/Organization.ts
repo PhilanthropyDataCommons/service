e37a4529aae563adf44dcf0d379ce2d0a6f04916
Dan Schultz <slifty@gmail.com>
2024-03-15 16:02:52 -0400

    Remove unused organization schema
    This schema isn't actually used anywhere, so let's take it out

--- src/types/Organization.ts
+++ src/types/Organization.ts
@@ -11,27 +11,6 @@ interface Organization {
 
 type WritableOrganization = Writable<Organization>;
 
-const organizationSchema: JSONSchemaType<Organization> = {
-	type: 'object',
-	properties: {
-		id: {
-			type: 'integer',
-		},
-		employerIdentificationNumber: {
-			type: 'string',
-		},
-		name: {
-			type: 'string',
-		},
-		createdAt: {
-			type: 'object',
-			required: [],
-			instanceof: 'Date',
-		},
-	},
-	required: ['id', 'employerIdentificationNumber', 'name', 'createdAt'],
-};
-
 const writableOrganizationSchema: JSONSchemaType<WritableOrganization> = {
 	type: 'object',
 	properties: {
@@ -45,14 +24,11 @@ const writableOrganizationSchema: JSONSchemaType<WritableOrganization> = {
 	required: ['employerIdentificationNumber', 'name'],
 };
 
-const isOrganization = ajv.compile(organizationSchema);
 const isWritableOrganization = ajv.compile(writableOrganizationSchema);
 
 export {
-	isOrganization,
 	isWritableOrganization,
 	Organization,
-	organizationSchema,
 	WritableOrganization,
 	writableOrganizationSchema,
 };

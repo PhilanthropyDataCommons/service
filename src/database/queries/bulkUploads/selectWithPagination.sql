2e1fe7f154e9f9b549d35980f8c675cb2d415b2b
Dan Schultz <slifty@gmail.com>
2024-04-10 14:28:23 -0400

    Only return authorized bulk uploads
    We now have the concept of bulk upload ownership and so we can change
    the behavior of our API to only return the bulk uploads associated with
    the current user.  Prior to this commit all users could see all other
    user's bulk uploads.
    
    Issue #759 Only return bulk uploads that the logged in user is authorized to view

--- src/database/queries/bulkUploads/selectWithPagination.sql
+++ src/database/queries/bulkUploads/selectWithPagination.sql
@@ -1,5 +1,12 @@
 SELECT bulk_upload_to_json(bulk_uploads.*) as "object"
 FROM bulk_uploads
+WHERE
+  CASE
+    WHEN :createdBy != 0 THEN
+      bulk_uploads.created_by = :createdBy
+    ELSE
+      true
+    END
 ORDER BY id DESC
 LIMIT
   CASE WHEN :limit != 0 THEN

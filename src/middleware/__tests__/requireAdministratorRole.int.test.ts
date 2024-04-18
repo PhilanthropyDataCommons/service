4a6f4ad51bb7747b62ea02062fb91c4d8d82d6de
Dan Schultz <slifty@gmail.com>
2024-04-18 15:39:17 -0400

    Add middleware for requiring administrator role
    This middleware will be used for any endpoint that is ONLY available to
    administrators. There may be some endpoints where admin status provides
    modified behavior, but this is intended to be a more explicit protection
    for particularly sensitive routes.
    
    Issue #929 Restrict POST /baseFields endpoint to admins only

---
+++ src/middleware/__tests__/requireAdministratorRole.int.test.ts
@@ -0,0 +1,50 @@
+import { requireAdministratorRole } from '../requireAdministratorRole';
+import { UnauthorizedError } from '../../errors';
+import type { Response } from 'express';
+import type { Request as JWTRequest } from 'express-jwt';
+
+describe('requireAuthentication', () => {
+	it('calls next with an UnauthorizedError when no roles value is provided', (done) => {
+		const mockRequest = {} as unknown as JWTRequest;
+		const mockResponse = {} as unknown as Response;
+		const nextMock = jest.fn((error) => {
+			expect(error).toBeInstanceOf(UnauthorizedError);
+			expect((error as UnauthorizedError).message).toEqual(
+				'Your account must have the administrator role.',
+			);
+			done();
+		});
+		requireAdministratorRole(mockRequest, mockResponse, nextMock);
+	});
+
+	it('calls next with an UnauthorizedError when the user has an administrator role set to false', (done) => {
+		const mockRequest = {
+			role: {
+				isAdministrator: false,
+			},
+		} as unknown as JWTRequest;
+		const mockResponse = {} as unknown as Response;
+		const nextMock = jest.fn((error) => {
+			expect(error).toBeInstanceOf(UnauthorizedError);
+			expect((error as UnauthorizedError).message).toEqual(
+				'Your account must have the administrator role.',
+			);
+			done();
+		});
+		requireAdministratorRole(mockRequest, mockResponse, nextMock);
+	});
+
+	it('calls next when the user has an administrator role set to true', (done) => {
+		const mockRequest = {
+			role: {
+				isAdministrator: true,
+			},
+		} as unknown as JWTRequest;
+		const mockResponse = {} as unknown as Response;
+		const nextMock = jest.fn((error) => {
+			expect(error).toBe(undefined);
+			done();
+		});
+		requireAdministratorRole(mockRequest, mockResponse, nextMock);
+	});
+});

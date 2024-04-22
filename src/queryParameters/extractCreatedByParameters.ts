PhilanthropyDataCommons/pdc

Add createdBy parameter to `/proposals`
This adds the ability to specify a createdBy filter when loading
proposals.  It also improves the way our `loadProposal` operation
and query work so that the authorization context is used in a more
complete way.

Issue #928 Add ability to filter by creator
Issue #927 Allow administrative users to view all data regardless of ownership

src/queryParameters/extractCreatedByParameters.ts
@@ -1,0 +1,42 @@
+import { ajv } from '../ajv';
+import { InputValidationError } from '../errors';
+import type { JSONSchemaType } from 'ajv';
+import type { Request } from 'express';
+
+interface CreatedByParameters {
+	createdBy: number | undefined;
+}
+
+interface CreatedByParametersQuery {
+	createdBy: number | undefined;
+}
+
+const createdByParametersQuerySchema: JSONSchemaType<CreatedByParametersQuery> =
+	{
+		type: 'object',
+		properties: {
+			createdBy: {
+				type: 'integer',
+				minimum: 1,
+				nullable: true,
+			},
+		},
+		required: [],
+	};
+
+const isCreatedByParametersQuery = ajv.compile(createdByParametersQuerySchema);
+
+const extractCreatedByParameters = (request: Request): CreatedByParameters => {
+	const { query } = request;
+	if (!isCreatedByParametersQuery(query)) {
+		throw new InputValidationError(
+			'Invalid createdBy parameters.',
+			isCreatedByParametersQuery.errors ?? [],
+		);
+	}
+	return {
+		createdBy: query.createdBy,
+	};
+};
+
+export { extractCreatedByParameters };

PhilanthropyDataCommons/service

src/queryParameters/extractCreatedByParameters.ts
@@ -1,0 +1,37 @@
+import { ajv } from '../ajv';
+import { InputValidationError } from '../errors';
+import type { JSONSchemaType } from 'ajv';
+import type { Request } from 'express';
+
+interface CreatedByParameters {
+	createdBy: number | undefined;
+}
+
+const createdByParametersSchema: JSONSchemaType<CreatedByParameters> = {
+	type: 'object',
+	properties: {
+		createdBy: {
+			type: 'integer',
+			minimum: 1,
+			nullable: true,
+		},
+	},
+	required: [],
+};
+
+const isCreatedByParameters = ajv.compile(createdByParametersSchema);
+
+const extractCreatedByParameters = (request: Request): CreatedByParameters => {
+	const { query } = request;
+	if (!isCreatedByParameters(query)) {
+		throw new InputValidationError(
+			'Invalid createdBy parameters.',
+			isCreatedByParameters.errors ?? [],
+		);
+	}
+	return {
+		createdBy: query.createdBy,
+	};
+};
+
+export { extractCreatedByParameters };
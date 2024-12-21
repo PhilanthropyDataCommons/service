2f945c7e69a223ad98f15aa38c29fdefe836b666
Jason Owen <jason@jasonaowen.net>
2024-12-20 19:14:32 -0800

    Reindent SQL with line break changes
    As the first part of enabling the SQLFluff layout.indent rule[1],
    apply the changes from running it that result in layout changes rather
    than merely reindentation. There should be no semantic differences here,
    just where lines are broken up.
    
    For simplicity, files with such changes do include the reindentation
    changes as well; splitting them up would leave some files with mixed
    indentation.
    
    [1] https://docs.sqlfluff.com/en/stable/reference/rules.html#rule-layout.indent

--- src/database/queries/userChangemakerPermissions/selectByPrimaryKey.sql
+++ src/database/queries/userChangemakerPermissions/selectByPrimaryKey.sql
@@ -1,8 +1,10 @@
-SELECT user_changemaker_permission_to_json(
-  user_changemaker_permissions.*
-) AS object
+SELECT
+	user_changemaker_permission_to_json(
+		user_changemaker_permissions.*
+	) AS object
 FROM user_changemaker_permissions
-WHERE user_keycloak_user_id = :userKeycloakUserId
-  AND changemaker_id = :changemakerId
-  AND permission = :permission
-  AND NOT is_expired(not_after);
+WHERE
+	user_keycloak_user_id = :userKeycloakUserId
+	AND changemaker_id = :changemakerId
+	AND permission = :permission
+	AND NOT is_expired(not_after);

d22398808fdf71f1454cad6ed44212d738f85aff
Jason Owen <jason@jasonaowen.net>
2024-12-20 19:14:32 -0800

    Reindent SQL with line break changes
    As the first part of enabling the SQLFluff layout.indent rule[1],
    apply the changes from running it that result in layout changes rather
    than merely reindentation. There should be no semantic differences here,
    just where lines are broken up.
    
    For simplicity, files with such changes do include the reindentation
    changes as well; splitting them up would leave some files with mixed
    indentation.
    
    [1] https://docs.sqlfluff.com/en/stable/reference/rules.html#rule-layout.indent

--- src/database/queries/proposals/selectWithPagination.sql
+++ src/database/queries/proposals/selectWithPagination.sql
@@ -1,37 +1,39 @@
 SELECT proposal_to_json(p.*) AS object
 FROM proposals AS p
-  LEFT JOIN proposal_versions AS pv ON p.id = pv.proposal_id
-  LEFT JOIN proposal_field_values AS pfv ON pv.id = pfv.proposal_version_id
-  LEFT JOIN changemakers_proposals AS op ON p.id = op.proposal_id
+	LEFT JOIN proposal_versions AS pv ON p.id = pv.proposal_id
+	LEFT JOIN proposal_field_values AS pfv ON pv.id = pfv.proposal_version_id
+	LEFT JOIN changemakers_proposals AS op ON p.id = op.proposal_id
 WHERE
-  CASE
-    WHEN :createdBy::uuid IS NULL THEN
-      TRUE
-    ELSE
-      p.created_by = :createdBy
-    END
-  AND CASE
-    WHEN (:search::text IS NULL
-      OR :search = '') THEN
-      TRUE
-    ELSE
-      pfv.value_search @@ websearch_to_tsquery('english', :search::text)
-    END
-  AND CASE
-    WHEN :changemakerId::integer IS NULL THEN
-      TRUE
-    ELSE
-      op.changemaker_id = :changemakerId
-    END
-  AND CASE
-    WHEN :authContextKeycloakUserId::uuid IS NULL THEN
-      TRUE
-    ELSE
-      (
-        p.created_by = :authContextKeycloakUserId
-        OR :authContextIsAdministrator::boolean
-      )
-    END
+	CASE
+		WHEN :createdBy::uuid IS NULL THEN
+			TRUE
+		ELSE
+			p.created_by = :createdBy
+	END
+	AND CASE
+		WHEN (
+			:search::text IS NULL
+			OR :search = ''
+		) THEN
+			TRUE
+		ELSE
+			pfv.value_search @@ websearch_to_tsquery('english', :search::text)
+	END
+	AND CASE
+		WHEN :changemakerId::integer IS NULL THEN
+			TRUE
+		ELSE
+			op.changemaker_id = :changemakerId
+	END
+	AND CASE
+		WHEN :authContextKeycloakUserId::uuid IS NULL THEN
+			TRUE
+		ELSE
+			(
+				p.created_by = :authContextKeycloakUserId
+				OR :authContextIsAdministrator::boolean
+			)
+	END
 GROUP BY p.id
 ORDER BY p.id DESC
 LIMIT :limit OFFSET :offset;
--- src/database/queries/userChangemakerPermissions/deleteOne.sql
+++ src/database/queries/userChangemakerPermissions/deleteOne.sql
@@ -1,6 +1,7 @@
 UPDATE user_changemaker_permissions
-  SET not_after = now()
-  WHERE user_keycloak_user_id = :userKeycloakUserId
-  AND permission = :permission::permission_t
-  AND changemaker_id = :changemakerId
-  AND NOT is_expired(not_after);
+SET not_after = now()
+WHERE
+	user_keycloak_user_id = :userKeycloakUserId
+	AND permission = :permission::permission_t
+	AND changemaker_id = :changemakerId
+	AND NOT is_expired(not_after);

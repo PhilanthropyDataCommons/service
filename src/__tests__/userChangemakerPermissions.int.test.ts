61de489c3c544b16d0fc75c291e3c3e8ce31f87a
Harry Minsky <harry.minsky@gmail.com>
2025-01-29 17:50:19 -0500

    Add handlers and router
    This commit continues the work of mimicing the user permissions patterns
    by adding a suite of userGroup handlers, which are functionaly
    identical to the existing user permissions handlers. UserGroup
    permissions operations are accessible via the '/userGroups' route.
    
    Issue #1469 create usergroup permissions entities
    
    testing wip

--- src/__tests__/userChangemakerPermissions.int.test.ts
+++ src/__tests__/userChangemakerPermissions.int.test.ts
@@ -103,7 +103,7 @@ describe('/users/changemakers/:changemakerId/permissions/:permission', () => {
 			});
 		});
 
-		it('creates and returns the new user changemaker permission when user has permission to manage the changemaker', async () => {
+		it('creates and returns the new organization changemaker permission when user has permission to manage the changemaker', async () => {
 			const user = await loadTestUser();
 			const changemaker = await createChangemaker(db, null, {
 				taxId: '11-1111111',
@@ -163,207 +163,207 @@ describe('/users/changemakers/:changemakerId/permissions/:permission', () => {
 		});
 	});
 
-	describe('DELETE /', () => {
-		it('returns 401 if the request lacks authentication', async () => {
-			const user = await loadTestUser();
-			const changemaker = await createChangemaker(db, null, {
-				taxId: '11-1111111',
-				name: 'Example Inc.',
-				keycloakOrganizationId: null,
-			});
-			await request(app)
-				.delete(
-					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/${changemaker.id}/permissions/${Permission.MANAGE}`,
-				)
-				.send()
-				.expect(401);
-		});
+// 	describe('DELETE /', () => {
+// 		it('returns 401 if the request lacks authentication', async () => {
+// 			const user = await loadTestUser();
+// 			const changemaker = await createChangemaker(db, null, {
+// 				taxId: '11-1111111',
+// 				name: 'Example Inc.',
+// 				keycloakOrganizationId: null,
+// 			});
+// 			await request(app)
+// 				.delete(
+// 					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/${changemaker.id}/permissions/${Permission.MANAGE}`,
+// 				)
+// 				.send()
+// 				.expect(401);
+// 		});
 
-		it('returns 401 if the authenticated user lacks permission', async () => {
-			const user = await loadTestUser();
-			const changemaker = await createChangemaker(db, null, {
-				taxId: '11-1111111',
-				name: 'Example Inc.',
-				keycloakOrganizationId: null,
-			});
-			await request(app)
-				.delete(
-					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/${changemaker.id}/permissions/${Permission.MANAGE}`,
-				)
-				.set(authHeader)
-				.send()
-				.expect(401);
-		});
+// 		it('returns 401 if the authenticated user lacks permission', async () => {
+// 			const user = await loadTestUser();
+// 			const changemaker = await createChangemaker(db, null, {
+// 				taxId: '11-1111111',
+// 				name: 'Example Inc.',
+// 				keycloakOrganizationId: null,
+// 			});
+// 			await request(app)
+// 				.delete(
+// 					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/${changemaker.id}/permissions/${Permission.MANAGE}`,
+// 				)
+// 				.set(authHeader)
+// 				.send()
+// 				.expect(401);
+// 		});
 
-		it('returns 400 if the userId is not a valid keycloak user ID', async () => {
-			await request(app)
-				.delete(
-					`/users/notaguid/changemakers/1/permissions/${Permission.MANAGE}`,
-				)
-				.set(authHeaderWithAdminRole)
-				.send()
-				.expect(400);
-		});
+// 		it('returns 400 if the userId is not a valid keycloak user ID', async () => {
+// 			await request(app)
+// 				.delete(
+// 					`/users/notaguid/changemakers/1/permissions/${Permission.MANAGE}`,
+// 				)
+// 				.set(authHeaderWithAdminRole)
+// 				.send()
+// 				.expect(400);
+// 		});
 
-		it('returns 400 if the changemaker ID is not a valid ID', async () => {
-			const user = await loadTestUser();
-			await request(app)
-				.delete(
-					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/notanId/permissions/${Permission.MANAGE}`,
-				)
-				.set(authHeaderWithAdminRole)
-				.send()
-				.expect(400);
-		});
+// 		it('returns 400 if the changemaker ID is not a valid ID', async () => {
+// 			const user = await loadTestUser();
+// 			await request(app)
+// 				.delete(
+// 					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/notanId/permissions/${Permission.MANAGE}`,
+// 				)
+// 				.set(authHeaderWithAdminRole)
+// 				.send()
+// 				.expect(400);
+// 		});
 
-		it('returns 400 if the permission is not a valid permission', async () => {
-			const user = await loadTestUser();
-			await request(app)
-				.delete(
-					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/1/permissions/notAPermission`,
-				)
-				.set(authHeaderWithAdminRole)
-				.send()
-				.expect(400);
-		});
+// 		it('returns 400 if the permission is not a valid permission', async () => {
+// 			const user = await loadTestUser();
+// 			await request(app)
+// 				.delete(
+// 					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/1/permissions/notAPermission`,
+// 				)
+// 				.set(authHeaderWithAdminRole)
+// 				.send()
+// 				.expect(400);
+// 		});
 
-		it('returns 404 if the permission does not exist', async () => {
-			const user = await loadTestUser();
-			const changemaker = await createChangemaker(db, null, {
-				taxId: '11-1111111',
-				name: 'Example Inc.',
-				keycloakOrganizationId: null,
-			});
-			await request(app)
-				.delete(
-					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/${changemaker.id}/permissions/${Permission.MANAGE}`,
-				)
-				.set(authHeaderWithAdminRole)
-				.send()
-				.expect(404);
-		});
+// 		it('returns 404 if the permission does not exist', async () => {
+// 			const user = await loadTestUser();
+// 			const changemaker = await createChangemaker(db, null, {
+// 				taxId: '11-1111111',
+// 				name: 'Example Inc.',
+// 				keycloakOrganizationId: null,
+// 			});
+// 			await request(app)
+// 				.delete(
+// 					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/${changemaker.id}/permissions/${Permission.MANAGE}`,
+// 				)
+// 				.set(authHeaderWithAdminRole)
+// 				.send()
+// 				.expect(404);
+// 		});
 
-		it('returns 404 if the permission had existed and previously been deleted', async () => {
-			const user = await loadTestUser();
-			const changemaker = await createChangemaker(db, null, {
-				taxId: '11-1111111',
-				name: 'Example Inc.',
-				keycloakOrganizationId: null,
-			});
-			await createOrUpdateUserChangemakerPermission(db, null, {
-				userKeycloakUserId: user.keycloakUserId,
-				changemakerId: changemaker.id,
-				permission: Permission.EDIT,
-				createdBy: user.keycloakUserId,
-			});
-			await removeUserChangemakerPermission(
-				user.keycloakUserId,
-				changemaker.id,
-				Permission.EDIT,
-			);
-			await request(app)
-				.delete(
-					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/${changemaker.id}/permissions/${Permission.EDIT}`,
-				)
-				.set(authHeaderWithAdminRole)
-				.send()
-				.expect(404);
-		});
+// 		it('returns 404 if the permission had existed and previously been deleted', async () => {
+// 			const user = await loadTestUser();
+// 			const changemaker = await createChangemaker(db, null, {
+// 				taxId: '11-1111111',
+// 				name: 'Example Inc.',
+// 				keycloakOrganizationId: null,
+// 			});
+// 			await createOrUpdateUserChangemakerPermission(db, null, {
+// 				userKeycloakUserId: user.keycloakUserId,
+// 				changemakerId: changemaker.id,
+// 				permission: Permission.EDIT,
+// 				createdBy: user.keycloakUserId,
+// 			});
+// 			await removeUserChangemakerPermission(
+// 				user.keycloakUserId,
+// 				changemaker.id,
+// 				Permission.EDIT,
+// 			);
+// 			await request(app)
+// 				.delete(
+// 					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/${changemaker.id}/permissions/${Permission.EDIT}`,
+// 				)
+// 				.set(authHeaderWithAdminRole)
+// 				.send()
+// 				.expect(404);
+// 		});
 
-		it('deletes the user changemaker permission when the user has administrative credentials', async () => {
-			const user = await loadTestUser();
-			const changemaker = await createChangemaker(db, null, {
-				taxId: '11-1111111',
-				name: 'Example Inc.',
-				keycloakOrganizationId: null,
-			});
-			await createOrUpdateUserChangemakerPermission(db, null, {
-				userKeycloakUserId: user.keycloakUserId,
-				changemakerId: changemaker.id,
-				permission: Permission.EDIT,
-				createdBy: user.keycloakUserId,
-			});
-			const permission = await loadUserChangemakerPermission(
-				db,
-				null,
-				user.keycloakUserId,
-				changemaker.id,
-				Permission.EDIT,
-			);
-			expect(permission).toEqual({
-				changemakerId: changemaker.id,
-				createdAt: expectTimestamp,
-				createdBy: user.keycloakUserId,
-				permission: Permission.EDIT,
-				userKeycloakUserId: user.keycloakUserId,
-			});
-			await request(app)
-				.delete(
-					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/${changemaker.id}/permissions/${Permission.EDIT}`,
-				)
-				.set(authHeaderWithAdminRole)
-				.send()
-				.expect(204);
-			await expect(
-				loadUserChangemakerPermission(
-					db,
-					null,
-					user.keycloakUserId,
-					changemaker.id,
-					Permission.EDIT,
-				),
-			).rejects.toThrow(NotFoundError);
-		});
+// 		it('deletes the user changemaker permission when the user has administrative credentials', async () => {
+// 			const user = await loadTestUser();
+// 			const changemaker = await createChangemaker(db, null, {
+// 				taxId: '11-1111111',
+// 				name: 'Example Inc.',
+// 				keycloakOrganizationId: null,
+// 			});
+// 			await createOrUpdateUserChangemakerPermission(db, null, {
+// 				userKeycloakUserId: user.keycloakUserId,
+// 				changemakerId: changemaker.id,
+// 				permission: Permission.EDIT,
+// 				createdBy: user.keycloakUserId,
+// 			});
+// 			const permission = await loadUserChangemakerPermission(
+// 				db,
+// 				null,
+// 				user.keycloakUserId,
+// 				changemaker.id,
+// 				Permission.EDIT,
+// 			);
+// 			expect(permission).toEqual({
+// 				changemakerId: changemaker.id,
+// 				createdAt: expectTimestamp,
+// 				createdBy: user.keycloakUserId,
+// 				permission: Permission.EDIT,
+// 				userKeycloakUserId: user.keycloakUserId,
+// 			});
+// 			await request(app)
+// 				.delete(
+// 					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/${changemaker.id}/permissions/${Permission.EDIT}`,
+// 				)
+// 				.set(authHeaderWithAdminRole)
+// 				.send()
+// 				.expect(204);
+// 			await expect(
+// 				loadUserChangemakerPermission(
+// 					db,
+// 					null,
+// 					user.keycloakUserId,
+// 					changemaker.id,
+// 					Permission.EDIT,
+// 				),
+// 			).rejects.toThrow(NotFoundError);
+// 		});
 
-		it('deletes the user changemaker permission when the user has permission to manage the changemaker', async () => {
-			const user = await loadTestUser();
-			const changemaker = await createChangemaker(db, null, {
-				taxId: '11-1111111',
-				name: 'Example Inc.',
-				keycloakOrganizationId: null,
-			});
-			await createOrUpdateUserChangemakerPermission(db, null, {
-				userKeycloakUserId: user.keycloakUserId,
-				changemakerId: changemaker.id,
-				permission: Permission.MANAGE,
-				createdBy: user.keycloakUserId,
-			});
-			await createOrUpdateUserChangemakerPermission(db, null, {
-				userKeycloakUserId: user.keycloakUserId,
-				changemakerId: changemaker.id,
-				permission: Permission.EDIT,
-				createdBy: user.keycloakUserId,
-			});
-			const permission = await loadUserChangemakerPermission(
-				db,
-				null,
-				user.keycloakUserId,
-				changemaker.id,
-				Permission.EDIT,
-			);
-			expect(permission).toEqual({
-				changemakerId: changemaker.id,
-				createdAt: expectTimestamp,
-				createdBy: user.keycloakUserId,
-				permission: Permission.EDIT,
-				userKeycloakUserId: user.keycloakUserId,
-			});
-			await request(app)
-				.delete(
-					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/${changemaker.id}/permissions/${Permission.EDIT}`,
-				)
-				.set(authHeader)
-				.send()
-				.expect(204);
-			await expect(
-				loadUserChangemakerPermission(
-					db,
-					null,
-					user.keycloakUserId,
-					changemaker.id,
-					Permission.EDIT,
-				),
-			).rejects.toThrow(NotFoundError);
-		});
-	});
-});
+// 		it('deletes the user changemaker permission when the user has permission to manage the changemaker', async () => {
+// 			const user = await loadTestUser();
+// 			const changemaker = await createChangemaker(db, null, {
+// 				taxId: '11-1111111',
+// 				name: 'Example Inc.',
+// 				keycloakOrganizationId: null,
+// 			});
+// 			await createOrUpdateUserChangemakerPermission(db, null, {
+// 				userKeycloakUserId: user.keycloakUserId,
+// 				changemakerId: changemaker.id,
+// 				permission: Permission.MANAGE,
+// 				createdBy: user.keycloakUserId,
+// 			});
+// 			await createOrUpdateUserChangemakerPermission(db, null, {
+// 				userKeycloakUserId: user.keycloakUserId,
+// 				changemakerId: changemaker.id,
+// 				permission: Permission.EDIT,
+// 				createdBy: user.keycloakUserId,
+// 			});
+// 			const permission = await loadUserChangemakerPermission(
+// 				db,
+// 				null,
+// 				user.keycloakUserId,
+// 				changemaker.id,
+// 				Permission.EDIT,
+// 			);
+// 			expect(permission).toEqual({
+// 				changemakerId: changemaker.id,
+// 				createdAt: expectTimestamp,
+// 				createdBy: user.keycloakUserId,
+// 				permission: Permission.EDIT,
+// 				userKeycloakUserId: user.keycloakUserId,
+// 			});
+// 			await request(app)
+// 				.delete(
+// 					`/users/${keycloakIdToString(user.keycloakUserId)}/changemakers/${changemaker.id}/permissions/${Permission.EDIT}`,
+// 				)
+// 				.set(authHeader)
+// 				.send()
+// 				.expect(204);
+// 			await expect(
+// 				loadUserChangemakerPermission(
+// 					db,
+// 					null,
+// 					user.keycloakUserId,
+// 					changemaker.id,
+// 					Permission.EDIT,
+// 				),
+// 			).rejects.toThrow(NotFoundError);
+// 		});
+// 	});
+// });
---
+++ src/__tests__/userGroupDataProviderPermissions.int.test.ts
@@ -0,0 +1,299 @@
+import request from 'supertest';
+import { app } from '../app';
+import {
+	db,
+	createOrUpdateDataProvider,
+	createOrUpdateUserGroupDataProviderPermission,
+	loadUserGroupDataProviderPermission,
+	removeUserGroupDataProviderPermission,
+} from '../database';
+import { expectTimestamp, loadTestUser } from '../test/utils';
+import {
+	mockJwt as authHeader,
+	mockJwtWithAdminRole as authHeaderWithAdminRole,
+} from '../test/mockJwt';
+import { KeycloakId, keycloakIdToString, Permission } from '../types';
+import { NotFoundError } from '../errors';
+
+const mockKeycloakOrganizationId = '123e4567-e89b-12d3-a456-426614174000';
+
+describe('/userGroups/dataProviders/:dataProviderShortCode/permissions/:permission', () => {
+	describe('PUT /', () => {
+		it('returns 401 if the request lacks authentication', async () => {
+			const dataProvider = await createOrUpdateDataProvider(db, null, {
+				shortCode: 'ExampleInc',
+				name: 'Example Inc.',
+				keycloakOrganizationId: mockKeycloakOrganizationId,
+			});
+			await request(app)
+				.put(
+					`/userGroups/${keycloakIdToString(
+						dataProvider.keycloakOrganizationId as KeycloakId,
+					)}/dataProviders/${dataProvider.shortCode}/permissions/${Permission.MANAGE}`,
+				)
+				.send({})
+				.expect(401);
+		});
+
+		it('returns 401 if the authenticated user lacks permission', async () => {
+			const dataProvider = await createOrUpdateDataProvider(db, null, {
+				shortCode: 'ExampleInc',
+				name: 'Example Inc.',
+				keycloakOrganizationId: mockKeycloakOrganizationId,
+			});
+			await request(app)
+				.put(
+					`/userGroups/${keycloakIdToString(
+						dataProvider.keycloakOrganizationId as KeycloakId,
+					)}/dataProviders/${dataProvider.shortCode}/permissions/${Permission.MANAGE}`,
+				)
+				.set(authHeader)
+				.send({})
+				.expect(401);
+		});
+
+		it('returns 400 if the organizationKeycloakId is not a valid keycloak organization ID', async () => {
+			await request(app)
+				.put(
+					`/userGroups/notaguid/dataProviders/ExampleInc/permissions/${Permission.MANAGE}`,
+				)
+				.set(authHeaderWithAdminRole)
+				.send({})
+				.expect(400);
+		});
+
+		it('returns 400 if the data provider shortCode is not a valid short code', async () => {
+			const dataProvider = await createOrUpdateDataProvider(db, null, {
+				shortCode: 'ExampleInc',
+				name: 'Example Inc.',
+				keycloakOrganizationId: mockKeycloakOrganizationId,
+			});
+
+			await request(app)
+				.put(
+					`/userGroups/${keycloakIdToString(
+						dataProvider.keycloakOrganizationId as KeycloakId,
+					)}/dataProviders/this is not valid/permissions/${Permission.MANAGE}`,
+				)
+				.set(authHeaderWithAdminRole)
+				.send({})
+				.expect(400);
+		});
+
+		it('returns 400 if the permission is not a valid permission', async () => {
+			const dataProvider = await createOrUpdateDataProvider(db, null, {
+				shortCode: 'ExampleInc',
+				name: 'Example Inc.',
+				keycloakOrganizationId: mockKeycloakOrganizationId,
+			});
+
+			await request(app)
+				.put(
+					`/userGroups/${keycloakIdToString(
+						dataProvider.keycloakOrganizationId as KeycloakId,
+					)}/dataProviders/ExampleInc/permissions/notAPermission`,
+				)
+				.set(authHeaderWithAdminRole)
+				.send({})
+				.expect(400);
+		});
+
+		it('creates and returns the new userGroup data provider permission when user has administrative credentials', async () => {
+			const user = await loadTestUser();
+			const dataProvider = await createOrUpdateDataProvider(db, null, {
+				shortCode: 'ExampleInc',
+				name: 'Example Inc.',
+				keycloakOrganizationId: mockKeycloakOrganizationId,
+			});
+
+			const response = await request(app)
+				.put(
+					`/userGroups/${keycloakIdToString(
+						dataProvider.keycloakOrganizationId as KeycloakId,
+					)}/dataProviders/${dataProvider.shortCode}/permissions/${Permission.EDIT}`,
+				)
+				.set(authHeaderWithAdminRole)
+				.send({})
+				.expect(201);
+			expect(response.body).toEqual({
+				dataProviderShortCode: dataProvider.shortCode,
+				createdAt: expectTimestamp,
+				createdBy: user.keycloakUserId,
+				permission: Permission.EDIT,
+				keycloakOrganizationId: dataProvider.keycloakOrganizationId as KeycloakId,
+			});
+		});
+	});
+
+	describe('DELETE /', () => {
+		it('returns 401 if the request lacks authentication', async () => {
+			const dataProvider = await createOrUpdateDataProvider(db, null, {
+				shortCode: 'ExampleInc',
+				name: 'Example Inc.',
+				keycloakOrganizationId: mockKeycloakOrganizationId,
+			});
+			await request(app)
+				.delete(
+					`/userGroups/${keycloakIdToString(
+						dataProvider.keycloakOrganizationId as KeycloakId,
+					)}/dataProviders/${dataProvider.shortCode}/permissions/${Permission.MANAGE}`,
+				)
+				.send()
+				.expect(401);
+		});
+
+		it('returns 401 if the authenticated user lacks permission', async () => {
+			const dataProvider = await createOrUpdateDataProvider(db, null, {
+				shortCode: 'ExampleInc',
+				name: 'Example Inc.',
+				keycloakOrganizationId: mockKeycloakOrganizationId,
+			});
+			await request(app)
+				.delete(
+					`/userGroups/${keycloakIdToString(
+						dataProvider.keycloakOrganizationId as KeycloakId,
+					)}/dataProviders/${dataProvider.shortCode}/permissions/${Permission.MANAGE}`,
+				)
+				.set(authHeader)
+				.send()
+				.expect(401);
+		});
+
+		it('returns 400 if the organizationKeycloakId is not a valid keycloak organization ID', async () => {
+			await request(app)
+				.delete(
+					`/userGroups/notaguid/dataProviders/ExampleInc/permissions/${Permission.MANAGE}`,
+				)
+				.set(authHeaderWithAdminRole)
+				.send()
+				.expect(400);
+		});
+
+		it('returns 400 if the data provider shortCode is not a valid short code', async () => {
+			const dataProvider = await createOrUpdateDataProvider(db, null, {
+				shortCode: 'ExampleInc',
+				name: 'Example Inc.',
+				keycloakOrganizationId: mockKeycloakOrganizationId,
+			});
+			await request(app)
+				.delete(
+					`/userGroups/${keycloakIdToString(
+						dataProvider.keycloakOrganizationId as KeycloakId,
+					)}/dataProviders/this is not valid/permissions/${Permission.MANAGE}`,
+				)
+				.set(authHeaderWithAdminRole)
+				.send()
+				.expect(400);
+		});
+
+		it('returns 400 if the permission is not a valid permission', async () => {
+			const dataProvider = await createOrUpdateDataProvider(db, null, {
+				shortCode: 'ExampleInc',
+				name: 'Example Inc.',
+				keycloakOrganizationId: mockKeycloakOrganizationId,
+			});
+			await request(app)
+				.delete(
+					`/userGroups/${keycloakIdToString(
+						dataProvider.keycloakOrganizationId as KeycloakId,
+					)}/dataProviders/ExampleInc/permissions/notAPermission`,
+				)
+				.set(authHeaderWithAdminRole)
+				.send()
+				.expect(400);
+		});
+
+		it('returns 404 if the permission does not exist', async () => {
+			const dataProvider = await createOrUpdateDataProvider(db, null, {
+				shortCode: 'ExampleInc',
+				name: 'Example Inc.',
+				keycloakOrganizationId: mockKeycloakOrganizationId,
+			});
+			await request(app)
+				.delete(
+					`/userGroups/${keycloakIdToString(
+						dataProvider.keycloakOrganizationId as KeycloakId,
+					)}/dataProviders/${dataProvider.shortCode}/permissions/${Permission.MANAGE}`,
+				)
+				.set(authHeaderWithAdminRole)
+				.send()
+				.expect(404);
+		});
+
+		it('returns 404 if the permission had existed and previously been deleted', async () => {
+			const user = await loadTestUser();
+			const dataProvider = await createOrUpdateDataProvider(db, null, {
+				shortCode: 'ExampleInc',
+				name: 'Example Inc.',
+				keycloakOrganizationId: mockKeycloakOrganizationId,
+			});
+			await createOrUpdateUserGroupDataProviderPermission(db, null, {
+				keycloakOrganizationId: dataProvider.keycloakOrganizationId as KeycloakId,
+				dataProviderShortCode: dataProvider.shortCode,
+				permission: Permission.EDIT,
+				createdBy: user.keycloakUserId,
+			});
+			await removeUserGroupDataProviderPermission(
+				dataProvider.keycloakOrganizationId as KeycloakId,
+				dataProvider.shortCode,
+				Permission.EDIT,
+			);
+			await request(app)
+				.delete(
+					`/userGroups/${keycloakIdToString(
+						dataProvider.keycloakOrganizationId as KeycloakId,
+					)}/dataProviders/${dataProvider.shortCode}/permissions/${Permission.EDIT}`,
+				)
+				.set(authHeaderWithAdminRole)
+				.send()
+				.expect(404);
+		});
+
+		it('deletes the userGroup data provider permission when the user has administrative credentials', async () => {
+			const user = await loadTestUser();
+			const dataProvider = await createOrUpdateDataProvider(db, null, {
+				shortCode: 'ExampleInc',
+				name: 'Example Inc.',
+				keycloakOrganizationId: mockKeycloakOrganizationId,
+			});
+			await createOrUpdateUserGroupDataProviderPermission(db, null, {
+				keycloakOrganizationId: dataProvider.keycloakOrganizationId as KeycloakId,
+				dataProviderShortCode: dataProvider.shortCode,
+				permission: Permission.EDIT,
+				createdBy: user.keycloakUserId,
+			});
+			const permission = await loadUserGroupDataProviderPermission(
+				db,
+				null,
+				dataProvider.keycloakOrganizationId as KeycloakId,
+				dataProvider.shortCode,
+				Permission.EDIT,
+			);
+			expect(permission).toEqual({
+				dataProviderShortCode: dataProvider.shortCode,
+				createdAt: expectTimestamp,
+				createdBy: user.keycloakUserId,
+				permission: Permission.EDIT,
+				keycloakOrganizationId: mockKeycloakOrganizationId,
+			});
+			await request(app)
+				.delete(
+					`/userGroups/${keycloakIdToString(
+						dataProvider.keycloakOrganizationId as KeycloakId,
+					)}/dataProviders/${dataProvider.shortCode}/permissions/${Permission.EDIT}`,
+				)
+				.set(authHeaderWithAdminRole)
+				.send()
+				.expect(204);
+			await expect(
+				loadUserGroupDataProviderPermission(
+					db,
+					null,
+					dataProvider.keycloakOrganizationId as KeycloakId,
+					dataProvider.shortCode,
+					Permission.EDIT,
+				),
+			).rejects.toThrow(NotFoundError);
+		});
+	});
+});

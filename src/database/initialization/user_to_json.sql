fe75765fa62c928e18e5328b1d6eba50ef260107
Dan Schultz <slifty@gmail.com>
2025-02-11 15:34:52 -0500

    Respect group permissions when populating user roles
    Now that we have user group permissions and the ability to associate
    a user with a given group we can incorporate those permissions in
    a user's roles.
    
    Issue #1483 Create user-group associations "cache" middleware

--- src/database/initialization/user_to_json.sql
+++ src/database/initialization/user_to_json.sql
@@ -10,40 +10,103 @@ DECLARE
 BEGIN
   user_changemaker_permissions_json := (
     SELECT jsonb_object_agg(
-      aggregated_user_changemaker_permissions.changemaker_id, aggregated_user_changemaker_permissions.permissions
+      aggregated_combined_changemaker_permissions.changemaker_id,
+      aggregated_combined_changemaker_permissions.permissions
     )
     FROM (
-      SELECT user_changemaker_permissions.changemaker_id, jsonb_agg(user_changemaker_permissions.permission) AS permissions
-      FROM user_changemaker_permissions
-      WHERE user_changemaker_permissions.user_keycloak_user_id = "user".keycloak_user_id
-        AND NOT is_expired(user_changemaker_permissions.not_after)
-      GROUP BY user_changemaker_permissions.changemaker_id
-    ) AS aggregated_user_changemaker_permissions
+      SELECT
+        combined_changemaker_permissions.changemaker_id,
+        jsonb_agg(combined_changemaker_permissions.permission) AS permissions
+      FROM (
+        (
+          SELECT
+            user_changemaker_permissions.changemaker_id AS changemaker_id,
+            user_changemaker_permissions.permission AS permission
+          FROM user_changemaker_permissions
+          WHERE user_changemaker_permissions.user_keycloak_user_id = "user".keycloak_user_id
+            AND NOT is_expired(user_changemaker_permissions.not_after)
+        )
+        UNION
+        (
+          SELECT
+            user_group_changemaker_permissions.changemaker_id AS changemaker_id,
+            user_group_changemaker_permissions.permission AS permission
+          FROM ephemeral_user_group_associations
+          JOIN user_group_changemaker_permissions
+          ON ephemeral_user_group_associations.user_group_keycloak_organization_id = user_group_changemaker_permissions.keycloak_organization_id
+          WHERE ephemeral_user_group_associations.user_keycloak_user_id = "user".keycloak_user_id
+            AND NOT is_expired(ephemeral_user_group_associations.not_after)
+        )
+      ) as combined_changemaker_permissions
+      GROUP BY combined_changemaker_permissions.changemaker_id
+    ) AS aggregated_combined_changemaker_permissions
   );
 
   user_data_provider_permissions_json := (
     SELECT jsonb_object_agg(
-      aggregated_user_data_provider_permissions.data_provider_short_code, aggregated_user_data_provider_permissions.permissions
+      aggregated_combined_data_provider_permissions.data_provider_short_code,
+      aggregated_combined_data_provider_permissions.permissions
     )
     FROM (
-      SELECT user_data_provider_permissions.data_provider_short_code, jsonb_agg(user_data_provider_permissions.permission) AS permissions
-      FROM user_data_provider_permissions
-      WHERE user_data_provider_permissions.user_keycloak_user_id = "user".keycloak_user_id
-        AND NOT is_expired(user_data_provider_permissions.not_after)
-      GROUP BY user_data_provider_permissions.data_provider_short_code
-    ) AS aggregated_user_data_provider_permissions );
+      SELECT
+        combined_data_provider_permissions.data_provider_short_code,
+        jsonb_agg(combined_data_provider_permissions.permission) AS permissions
+      FROM (
+        (
+          SELECT
+            user_data_provider_permissions.data_provider_short_code AS data_provider_short_code,
+            user_data_provider_permissions.permission AS permission
+          FROM user_data_provider_permissions
+          WHERE user_data_provider_permissions.user_keycloak_user_id = "user".keycloak_user_id
+            AND NOT is_expired(user_data_provider_permissions.not_after)
+        )
+        UNION
+        (
+          SELECT
+            user_group_data_provider_permissions.data_provider_short_code AS data_provider_short_code,
+            user_group_data_provider_permissions.permission AS permission
+          FROM ephemeral_user_group_associations
+          JOIN user_group_data_provider_permissions
+            ON ephemeral_user_group_associations.user_group_keycloak_organization_id = user_group_data_provider_permissions.keycloak_organization_id
+          WHERE ephemeral_user_group_associations.user_keycloak_user_id = "user".keycloak_user_id
+            AND NOT is_expired(ephemeral_user_group_associations.not_after)
+        )
+      ) as combined_data_provider_permissions
+      GROUP BY combined_data_provider_permissions.data_provider_short_code
+    ) AS aggregated_combined_data_provider_permissions );
 
   user_funder_permissions_json := (
     SELECT jsonb_object_agg(
-      aggregated_user_funder_permissions.funder_short_code, aggregated_user_funder_permissions.permissions
+      aggregated_combined_funder_permissions.funder_short_code,
+      aggregated_combined_funder_permissions.permissions
     )
     FROM (
-      SELECT user_funder_permissions.funder_short_code, jsonb_agg(user_funder_permissions.permission) AS permissions
-      FROM user_funder_permissions
-      WHERE user_funder_permissions.user_keycloak_user_id = "user".keycloak_user_id
-        AND NOT is_expired(user_funder_permissions.not_after)
-      GROUP BY user_funder_permissions.funder_short_code
-    ) AS aggregated_user_funder_permissions
+      SELECT
+        combined_funder_permissions.funder_short_code,
+        jsonb_agg(combined_funder_permissions.permission) AS permissions
+      FROM (
+        (
+          SELECT
+            user_funder_permissions.funder_short_code AS funder_short_code,
+            user_funder_permissions.permission AS permission
+          FROM user_funder_permissions
+          WHERE user_funder_permissions.user_keycloak_user_id = "user".keycloak_user_id
+            AND NOT is_expired(user_funder_permissions.not_after)
+        )
+        UNION
+        (
+          SELECT
+            user_group_funder_permissions.funder_short_code AS funder_short_code,
+            user_group_funder_permissions.permission AS permission
+          FROM ephemeral_user_group_associations
+          JOIN user_group_funder_permissions
+            ON ephemeral_user_group_associations.user_group_keycloak_organization_id = user_group_funder_permissions.keycloak_organization_id
+          WHERE ephemeral_user_group_associations.user_keycloak_user_id = "user".keycloak_user_id
+            AND NOT is_expired(ephemeral_user_group_associations.not_after)
+        )
+      ) as combined_funder_permissions
+      GROUP BY combined_funder_permissions.funder_short_code
+    ) AS aggregated_combined_funder_permissions
   );
 
   permissions_json := jsonb_build_object(

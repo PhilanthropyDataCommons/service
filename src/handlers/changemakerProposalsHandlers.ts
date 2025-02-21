5180a13f6ae18bacefd76e63fd007adf1bfdf19c
Dan Schultz <slifty@gmail.com>
2025-02-21 14:49:12 -0500

    Add permission check for ProposalChangemaker creation
    Funders can associate any proposals that exist against their
    opportunities with changemakers
    
    Issue #1406 Utilize permissions

--- src/handlers/changemakerProposalsHandlers.ts
+++ src/handlers/changemakerProposalsHandlers.ts
@@ -3,17 +3,28 @@ import {
 	createChangemakerProposal,
 	getLimitValues,
 	loadChangemakerProposalBundle,
+	loadProposal,
+	loadOpportunity,
 } from '../database';
 import {
+	isAuthContext,
 	isTinyPgErrorWithQueryContext,
 	isWritableChangemakerProposal,
+	Permission,
 } from '../types';
-import { DatabaseError, InputValidationError } from '../errors';
+import {
+	DatabaseError,
+	FailedMiddlewareError,
+	InputValidationError,
+	NotFoundError,
+	UnprocessableEntityError,
+} from '../errors';
 import {
 	extractChangemakerParameters,
 	extractProposalParameters,
 	extractPaginationParameters,
 } from '../queryParameters';
+import { authContextHasFunderPermission } from '../authorization';
 import type { Request, Response, NextFunction } from 'express';
 
 const getChangemakerProposals = (
@@ -53,6 +64,10 @@ const postChangemakerProposal = (
 	res: Response,
 	next: NextFunction,
 ): void => {
+	if (!isAuthContext(req)) {
+		next(new FailedMiddlewareError('Unexpected lack of auth context.'));
+		return;
+	}
 	if (!isWritableChangemakerProposal(req.body)) {
 		next(
 			new InputValidationError(
@@ -62,17 +77,38 @@ const postChangemakerProposal = (
 		);
 		return;
 	}
+	const { proposalId, changemakerId } = req.body;
 
 	(async () => {
-		const changemakerProposal = await createChangemakerProposal(
-			db,
-			null,
-			req.body,
-		);
+		const proposal = await loadProposal(db, null, proposalId);
+		const opportunity = await loadOpportunity(db, null, proposal.opportunityId);
+		if (
+			!authContextHasFunderPermission(
+				req,
+				opportunity.funderShortCode,
+				Permission.EDIT,
+			)
+		) {
+			throw new UnprocessableEntityError(
+				'You do not have write permissions on the funder associated with this proposal.',
+			);
+		}
+		const changemakerProposal = await createChangemakerProposal(db, null, {
+			changemakerId,
+			proposalId,
+		});
 		res.status(201).contentType('application/json').send(changemakerProposal);
 	})().catch((error: unknown) => {
 		if (isTinyPgErrorWithQueryContext(error)) {
 			next(new DatabaseError('Error creating changemaker proposal.', error));
+			return;
+		}
+		if (error instanceof NotFoundError) {
+			next(
+				new UnprocessableEntityError(
+					`related ${error.details.entityType} not found.`,
+				),
+			);
 			return;
 		}
 		next(error);

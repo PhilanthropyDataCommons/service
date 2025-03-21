c78cb6d1f49fd1df3a68abafbee36b32cd04d153
Dan Schultz <slifty@gmail.com>
2025-03-21 11:59:37 -0400

    Remove assertUserGroupDataProviderPermissionExists operation
    This operation is no longer needed now that our delete operations will
    throw NotFoundErrors consistently if a row was not returned.
    
    Issue #1547 Remove unnecessary assert operations

--- src/database/operations/userGroupDataProviderPermissions/index.ts
+++ src/database/operations/userGroupDataProviderPermissions/index.ts
@@ -1,4 +1,3 @@
-export * from './assertUserGroupDataProviderPermissionExists';
 export * from './createOrUpdateUserGroupDataProviderPermission';
 export * from './loadUserGroupDataProviderPermission';
 export * from './removeUserGroupDataProviderPermission';

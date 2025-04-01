7dfea2a9ba11410232a383cd85b7935ffa47eaf9
Dan Schultz <slifty@gmail.com>
2025-04-01 11:37:46 -0400

    Load openapi specification via http
    We served the swagger documentation page by passing a complete
    json object to the renderer on server start.  This had some optimization
    benefits, but it also limited how we could write our documentation.
    
    For example: we couldn't use yaml, we also couldn't split our
    documentation into multiple pieces.
    
    This change serves the specification and configures swagger-ui to load
    it dynamically.  Over time we will be breaking it into pieces and
    possibly switching to a yaml format.
    
    Because we're now serving the specification I had to change the way we
    inject our auth issuers; we aren't serving the api file directly but
    rather we serve a modified copy of its contents.
    
    Issue #1178 Break our openAPI json file into reusable pieces rather than a single monolith

---
+++ src/handlers/documentationHandlers.ts
@@ -0,0 +1,37 @@
+import path from 'path';
+import fs from 'fs/promises';
+import { issuer } from '../auth/jwtOptions';
+import type { Request, Response } from 'express';
+
+const readAndExpandDocumentationFile = async (relativeFilePath: string) => {
+	const absoluteFilePath = path.join(__dirname, '../openapi', relativeFilePath);
+	const rawContent = await fs.readFile(absoluteFilePath, 'utf8');
+	const expandedContent = rawContent.replace(/{{AUTH_ISSUER}}/g, issuer);
+	return expandedContent;
+};
+
+const expandedDocumentationCache: Record<string, string> = {};
+const getExpandedDocumentation = async (
+	relativeFilePath: string,
+): Promise<string> => {
+	const cachedValue = expandedDocumentationCache[relativeFilePath];
+	if (cachedValue) {
+		return cachedValue;
+	}
+	const expandedContent =
+		await readAndExpandDocumentationFile(relativeFilePath);
+	expandedDocumentationCache[relativeFilePath] = expandedContent;
+	return expandedContent;
+};
+
+const getRootApiSpec = async (req: Request, res: Response) => {
+	const expandedDocumentation = await getExpandedDocumentation('api.json');
+	res.type('application/json');
+	res.set(
+		'Content-Length',
+		Buffer.byteLength(expandedDocumentation, 'utf8').toString(),
+	);
+	res.send(expandedDocumentation);
+};
+
+export const documentationHandlers = { getRootApiSpec };

7d9bf7e8b859041f1514fc50f80b09eba470414c
Dan Schultz <slifty@gmail.com>
2025-04-28 11:15:25 -0400

    Specify UTC on all connections
    We should have our service return timestamps as UTC rather than whatever
    the host has been configured with.  The absolute times are all the same,
    so this is mostly relevant for tests but consistency in output is
    useful.
    
    Note that the service will still accept timestamps in any absolute time.

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,13 +4,19 @@ import { TinyPg } from 'tinypg';
 import { getLogger } from '../logger';
 
 const initializationDirectory = path.join(__dirname, 'initialization');
+const logger = getLogger('db');
 
 const db = new TinyPg({
 	root_dir: [path.resolve(__dirname, 'queries')],
 });
 
+db.pool.on('connect', (client) => {
+	client.query("SET TIME ZONE 'UTC';").catch((err: unknown) => {
+		logger.error({ err }, 'Failed to set timezone');
+	});
+});
+
 const initializeDatabase = async () => {
-	const logger = getLogger('initializeDatabase');
 	const initializationFiles = (
 		await fs.readdir(initializationDirectory)
 	).filter((file) => file.endsWith('.sql'));
@@ -18,7 +24,6 @@ const initializeDatabase = async () => {
 	await Promise.all(
 		initializationFiles.map(async (file) => {
 			const filePath = path.join(initializationDirectory, file);
-			logger.debug({ filePath }, 'Running SQL file');
 			const sql = (await fs.readFile(filePath)).toString();
 			await db.query(sql);
 		}),

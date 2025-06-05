0f8dd8368f794d0566e3b6117180fc7e7bb4d3ee
Dan Schultz <slifty@gmail.com>
2025-06-05 16:16:06 -0400

    Omit forbidden data from proposal versions
    If a base field is forbidden we do not want to expose field values
    associated with it.  This change removes those field values when loading
    proposal versions at the database level.
    
    Issue #1621 Add the concept of "too sensitive to store data" to base fields

--- src/__tests__/proposals.int.test.ts
+++ src/__tests__/proposals.int.test.ts
@@ -1220,6 +1220,126 @@ describe('/proposals', () => {
 				],
 			});
 		});
+
+		it('does not return proposal field values associated with forbidden base fields', async () => {
+			const testUser = await loadTestUser();
+			const testUserAuthContext = getAuthContext(testUser);
+			const systemSource = await loadSystemSource(db, null);
+			const systemFunder = await loadSystemFunder(db, null);
+			await createTestBaseFields();
+			const forbiddenBaseField = await createOrUpdateBaseField(db, null, {
+				shortCode: 'forbiddenField',
+				dataType: BaseFieldDataType.STRING,
+				label: 'Forbidden Field',
+				description: 'This field should not be returned',
+				scope: BaseFieldScope.PROPOSAL,
+				valueRelevanceHours: null,
+				sensitivityClassification: BaseFieldSensitivityClassification.FORBIDDEN,
+			});
+			await createOpportunity(db, null, {
+				title: '🌎',
+				funderShortCode: systemFunder.shortCode,
+			});
+			await createApplicationForm(db, null, {
+				opportunityId: 1,
+			});
+			await createApplicationFormField(db, null, {
+				applicationFormId: 1,
+				baseFieldShortCode: 'title',
+				position: 1,
+				label: 'Short summary or title',
+			});
+			const forbiddenApplicationFormField = await createApplicationFormField(
+				db,
+				null,
+				{
+					applicationFormId: 1,
+					baseFieldShortCode: forbiddenBaseField.shortCode,
+					position: 2,
+					label: 'forbidden field',
+				},
+			);
+			await createProposal(db, testUserAuthContext, {
+				externalId: `proposal-2525-01-04T00Z`,
+				opportunityId: 1,
+			});
+			await createProposalVersion(db, testUserAuthContext, {
+				proposalId: 1,
+				applicationFormId: 1,
+				sourceId: systemSource.id,
+			});
+			await createProposalFieldValue(db, null, {
+				proposalVersionId: 1,
+				applicationFormFieldId: 1,
+				position: 1,
+				value: 'Title for version 1 from 2525-01-04',
+				isValid: true,
+				goodAsOf: null,
+			});
+			await createProposalFieldValue(db, null, {
+				proposalVersionId: 1,
+				applicationFormFieldId: forbiddenApplicationFormField.id,
+				position: 1,
+				value: 'Should not be returned',
+				isValid: true,
+				goodAsOf: null,
+			});
+			const response = await request(app)
+				.get('/proposals/1')
+				.set(authHeaderWithAdminRole)
+				.expect(200);
+			expect(response.body).toEqual({
+				id: 1,
+				opportunityId: 1,
+				externalId: 'proposal-2525-01-04T00Z',
+				createdAt: expectTimestamp,
+				createdBy: testUser.keycloakUserId,
+				versions: [
+					{
+						id: 1,
+						proposalId: 1,
+						sourceId: systemSource.id,
+						source: systemSource,
+						applicationFormId: 1,
+						version: 1,
+						createdAt: expectTimestamp,
+						createdBy: testUser.keycloakUserId,
+						fieldValues: [
+							{
+								id: 1,
+								proposalVersionId: 1,
+								applicationFormFieldId: 1,
+								position: 1,
+								value: 'Title for version 1 from 2525-01-04',
+								createdAt: expectTimestamp,
+								isValid: true,
+								goodAsOf: null,
+								applicationFormField: {
+									id: 1,
+									applicationFormId: 1,
+									baseFieldShortCode: 'title',
+									baseField: {
+										createdAt: expectTimestamp,
+										dataType: 'string',
+										description: 'The title of the proposal',
+										label: 'Title',
+										scope: 'proposal',
+										valueRelevanceHours: null,
+										sensitivityClassification:
+											BaseFieldSensitivityClassification.RESTRICTED,
+										shortCode: 'title',
+										localizations: {},
+									},
+									position: 1,
+									label: 'Short summary or title',
+									createdAt: expectTimestamp,
+								},
+							},
+						],
+					},
+				],
+			});
+		});
 	});
 
 	describe('POST /', () => {

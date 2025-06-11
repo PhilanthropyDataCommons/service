b440a69ccbc6053c57e8d4b11b7782b8f8b353d4
Dan Schultz <slifty@gmail.com>
2025-06-11 14:16:01 -0400

    Set up `love` linting rules
    We dropped airbnb because it wasn't supported, but look at this amazing
    set of rules! In addition to being actively maintained these rules also
    appear more robust (as evidenced by the number of rules we needed to
    disable after using the ruleset).
    
    By keeping those rules disabled we are set up to be able to go through
    each and either enable it or move the exception to the primary block
    where rules are manually reconfigured.
    
    Issue #1699 Move from airbnb eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,65 @@
 import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
+import love from 'eslint-config-love';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
-import importPlugin from 'eslint-plugin-import';
 
 import sortExports from 'eslint-plugin-sort-exports';
 import globals from 'globals';
 
 export default defineConfig([
-	{
-		files: ['**/*.ts'],
-	},
 	js.configs.recommended,
 	tseslint.configs.eslintRecommended,
 	tseslint.configs.recommendedTypeChecked,
 	tseslint.configs.strict,
-	importPlugin.flatConfigs.recommended,
+	{
+		...love,
+		languageOptions: {
+			parserOptions: {
+				project: './tsconfig.dev.json',
+			},
+		},
+	},
 	prettier,
+	{
+		// These are new love rules that we weren't following.  Disabling them here lets
+		// us re-enable them one-by-one alongside any necessary code changes.
+		rules: {
+			'@typescript-eslint/array-type': 'off',
+			'@typescript-eslint/consistent-generic-constructors': 'off',
+			'@typescript-eslint/consistent-indexed-object-style': 'off',
+			'@typescript-eslint/consistent-type-assertions': 'off',
+			'@typescript-eslint/consistent-type-exports': 'off',
+			'@typescript-eslint/consistent-type-imports': 'off',
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'@typescript-eslint/init-declarations': 'off',
+			'@typescript-eslint/no-confusing-void-expression': 'off',
+			'@typescript-eslint/no-deprecated': 'off',
+			'@typescript-eslint/no-empty-function': 'off',
+			'@typescript-eslint/no-import-type-side-effects': 'off',
+			'@typescript-eslint/no-magic-numbers': 'off',
+			'@typescript-eslint/no-unnecessary-boolean-literal-compare': 'off',
+			'@typescript-eslint/no-unnecessary-condition': 'off',
+			'@typescript-eslint/no-unnecessary-template-expression': 'off',
+			'@typescript-eslint/no-unnecessary-type-parameters': 'off',
+			'@typescript-eslint/no-unsafe-type-assertion': 'off',
+			'@typescript-eslint/non-nullable-type-assertion-style': 'off',
+			'@typescript-eslint/prefer-destructuring': 'off',
+			'@typescript-eslint/prefer-optional-chain': 'off',
+			'@typescript-eslint/promise-function-async': 'off',
+			'@typescript-eslint/return-await': 'off',
+			'@typescript-eslint/strict-boolean-expressions': 'off',
+			'@typescript-eslint/use-unknown-in-catch-callback-variable': 'off',
+			'eslint-comments/require-description': 'off',
+			'import/first': 'off',
+			'max-lines': 'off',
+			'max-nested-callbacks': 'off',
+			'n/no-path-concat': 'off',
+			'no-unreachable': 'off',
+			'promise/avoid-new': 'off',
+			complexity: 'off',
+		},
+	},
 	{
 		plugins: {
 			'sort-exports': sortExports,

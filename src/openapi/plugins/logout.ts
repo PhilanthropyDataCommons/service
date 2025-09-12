d8bd056db86de869b60290894d68bd759fac03a5
Jesse Bickel <jbickel@opentechstrategies.com>
2025-09-12 10:37:15 -0500

    Feeble attempt at satisfying ESLint complaints
    This does not solve them.
    
    Either this approach of a separate module needs to be changed or we
    need a very nuanced and fancy eslint/tsconfig change.

--- src/openapi/plugins/logout.ts
+++ src/openapi/plugins/logout.ts
@@ -1,4 +1,4 @@
-const logout = (system: { 
+const logout = (system: {
 	getState: () => Map<string,Map<string,Map<string,Map<string,Map<string,string>>>>>,
 	specSelectors: { specJson: () => Map<string,Map<string,Map<string,Map<string,Map<string,Map<string,string>>>>>> }
 }) => ({
@@ -6,13 +6,14 @@ const logout = (system: {
 		auth: {
 			wrapActions: {
 				logout: (originalFunction: (keys: string[]) => Map<string,object>) => (keys: string[]) => {
+					const { location } = window as Window;
 					const spec = system.specSelectors.specJson();
 					const logoutUrl = spec.get('components')?.get('securitySchemes')?.get('auth')?.get('flows')?.get('authorizationCode')?.get('logoutUrl');
 					const idToken = system.getState().get('auth')?.get('authorized')?.get('auth')?.get('token')?.get('id_token');
 					const currentLocation = window.location.href;
 					const result = originalFunction(keys);
 					if (logoutUrl !== undefined) {
-						window.location.href = `${logoutUrl}?&id_token_hint=${idToken}&post_logout_redirect_uri=${currentLocation}`;
+						location.href = `${logoutUrl}?&id_token_hint=${idToken}&post_logout_redirect_uri=${currentLocation}`;
 					}
 					return result;
 				}

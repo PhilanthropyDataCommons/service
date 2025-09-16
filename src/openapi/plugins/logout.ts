4c64efc15db4336f66db5a0fc63eeee460d488c2
Jesse Bickel <jbickel@opentechstrategies.com>
2025-09-16 11:16:22 -0500

    WIP - needs syntax fix

--- src/openapi/plugins/logout.ts
+++ src/openapi/plugins/logout.ts
@@ -1,13 +1,12 @@
 const logout = (system: {
 	getState: () => Map<string,Map<string,Map<string,Map<string,Map<string,string>>>>>,
 	specSelectors: { specJson: () => Map<string,Map<string,Map<string,Map<string,Map<string,Map<string,string>>>>>> }
-}) => ({
+}): {statePlugins: {auth: {wrapActions: {logout: (originalFunction: (keys: string[]) => Map<string,object> => (keys:string[]))}}}} =>  ({
 	statePlugins: {
 		auth: {
 			wrapActions: {
 				logout: (originalFunction: (keys: string[]) => Map<string,object>) => (keys: string[]) => {
-					const spec = system.specSelectors.specJson();
-					const logoutUrl = spec.get('components')?.get('securitySchemes')?.get('auth')?.get('flows')?.get('authorizationCode')?.get('logoutUrl');
+					const logoutUrl = system.specSelectors.specJson().get('components')?.get('securitySchemes')?.get('auth')?.get('flows')?.get('authorizationCode')?.get('logoutUrl');
 					const idToken = system.getState().get('auth')?.get('authorized')?.get('auth')?.get('token')?.get('id_token');
 					const { location: { href } } = window;
 					const result = originalFunction(keys);

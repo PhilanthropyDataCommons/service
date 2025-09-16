f9ecac9fe29d3d8b2a1c6e29529565f291b636d6
Jesse Bickel <jbickel@opentechstrategies.com>
2025-09-16 12:14:01 -0500

    "prettier" aka "uglier"

--- src/openapi/plugins/logout.ts
+++ src/openapi/plugins/logout.ts
@@ -1,22 +1,45 @@
-type DeepNestedMap = Map<string, Map<string, Map<string, Map<string, Map<string, Map<string, string>>>>>>;
-type NestedMap = Map<string, Map<string, Map<string, Map<string, Map<string, string>>>>>;
+type DeepNestedMap = Map<
+	string,
+	Map<string, Map<string, Map<string, Map<string, Map<string, string>>>>>
+>;
+type NestedMap = Map<
+	string,
+	Map<string, Map<string, Map<string, Map<string, string>>>>
+>;
 interface System {
 	getState: () => NestedMap;
 	specSelectors: {
 		specJson: () => DeepNestedMap;
 	};
-};
+}
 const getLogoutUrl = (system: System): string | undefined =>
-	system.specSelectors.specJson().get('components')?.get('securitySchemes')?.get('auth')?.get('flows')?.get('authorizationCode')?.get('logoutUrl');
+	system.specSelectors
+		.specJson()
+		.get('components')
+		?.get('securitySchemes')
+		?.get('auth')
+		?.get('flows')
+		?.get('authorizationCode')
+		?.get('logoutUrl');
 
 const getIdToken = (system: System): string | undefined =>
-	system.getState().get('auth')?.get('authorized')?.get('auth')?.get('token')?.get('id_token');
+	system
+		.getState()
+		.get('auth')
+		?.get('authorized')
+		?.get('auth')
+		?.get('token')
+		?.get('id_token');
 
-const logout = (system: System): {
+const logout = (
+	system: System,
+): {
 	statePlugins: {
 		auth: {
 			wrapActions: {
-				logout: (originalFunction: (keys: string[]) => Map<string, object>) => (keys: string[]) => Map<string, object>;
+				logout: (
+					originalFunction: (keys: string[]) => Map<string, object>,
+				) => (keys: string[]) => Map<string, object>;
 			};
 		};
 	};
@@ -24,16 +47,20 @@ const logout = (system: System): {
 	statePlugins: {
 		auth: {
 			wrapActions: {
-				logout: (originalFunction: (keys: string[]) => Map<string, object>) => (keys: string[]) => {
-					const logoutUrl = getLogoutUrl(system);
-					const idToken = getIdToken(system);
-					const { location: { href } } = window;
-					const result = originalFunction(keys);
-					if (logoutUrl !== undefined) {
-						location.href = `${logoutUrl}?&id_token_hint=${idToken}&post_logout_redirect_uri=${href}`;
-					}
-					return result;
-				},
+				logout:
+					(originalFunction: (keys: string[]) => Map<string, object>) =>
+					(keys: string[]) => {
+						const logoutUrl = getLogoutUrl(system);
+						const idToken = getIdToken(system);
+						const {
+							location: { href },
+						} = window;
+						const result = originalFunction(keys);
+						if (logoutUrl !== undefined) {
+							location.href = `${logoutUrl}?&id_token_hint=${idToken}&post_logout_redirect_uri=${href}`;
+						}
+						return result;
+					},
 			},
 		},
 	},

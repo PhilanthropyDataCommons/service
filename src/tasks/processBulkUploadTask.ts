09cc5de20980149f670071337fa3a3bf52602c76
Dan Schultz <slifty@gmail.com>
2025-09-17 15:59:09 -0400

    Standardize authContext for bulk upload
    The auth context of any task (particularly bulk upload task) is
    currently fabricated based on whoever created the bulk upload task.
    
    To load the bulk upload task we needed to load as a superuser (which is
    OK -- the task runner itself is a superuser), but we want to switch to
    the auth context of the bulk uploader as early as possible.  This change
    switches to using that context sooner, and also encapsulates the act of
    generating that context into its own function.

--- src/tasks/processBulkUploadTask.ts
+++ src/tasks/processBulkUploadTask.ts
@@ -33,7 +33,6 @@ import type { FileResult } from 'tmp-promise';
 import type {
 	ApplicationFormField,
 	BulkUploadTask,
-	Opportunity,
 	Changemaker,
 	ProposalFieldValue,
 	WritableChangemaker,
@@ -168,14 +167,6 @@ const assertBulkUploadTaskCsvIsValid = async (
 	await assertCsvContainsRowsOfEqualLength(csvPath);
 };
 
-const createOpportunityForBulkUploadTask = async (
-	bulkUploadTask: BulkUploadTask,
-): Promise<Opportunity> =>
-	await createOpportunity(db, null, {
-		title: `Bulk Upload (${bulkUploadTask.createdAt})`,
-		funderShortCode: bulkUploadTask.funderShortCode,
-	});
-
 const createApplicationFormFieldsForBulkUploadTask = async (
 	csvPath: string,
 	applicationFormId: number,
@@ -224,7 +215,7 @@ const createOrLoadChangemaker = async (
 		);
 	} catch {
 		if (writeValues.name !== undefined) {
-			return await createChangemaker(localDb, null, {
+			return await createChangemaker(localDb, authContext, {
 				...writeValues,
 				name: writeValues.name, // This looks silly, but TypeScript isn't guarding `writeValues`, just `writeValues.name`.
 			});
@@ -238,10 +229,19 @@ const createOrLoadChangemaker = async (
 // for the task runner.  Currently this means the task runner is functioning as an administrative system user.
 // This creates risk where a task could behave in ways with escalated privileges, although currently
 // the implementation of the bulk upload processer should be safe.
-const loadTaskRunnerAuthContext = async (): Promise<AuthContext> => ({
+const loadSystemUserAuthContext = async (): Promise<AuthContext> => ({
 	user: await loadSystemUser(db, null),
 	role: {
 		isAdministrator: true,
+	},
+});
+
+const loadTaskAuthContext = async (
+	bulkUploadTask: BulkUploadTask,
+): Promise<AuthContext> => ({
+	user: await loadUserByKeycloakUserId(db, null, bulkUploadTask.createdBy),
+	role: {
+		isAdministrator: false,
 	},
 });
 
@@ -256,15 +256,17 @@ export const processBulkUploadTask = async (
 		});
 		return;
 	}
-	const taskRunnerAuthContext = await loadTaskRunnerAuthContext();
+	const systemUserAuthContext = await loadSystemUserAuthContext();
 	graphileLogger.debug(
 		`Started processBulkUpload Job for Bulk Upload ID ${payload.bulkUploadId}`,
 	);
 	const bulkUploadTask = await loadBulkUploadTask(
 		db,
-		taskRunnerAuthContext,
+		systemUserAuthContext,
 		payload.bulkUploadId,
 	);
+
+	const taskAuthContext = await loadTaskAuthContext(bulkUploadTask);
 	if (bulkUploadTask.status !== TaskStatus.PENDING) {
 		graphileLogger.warn(
 			'Bulk upload cannot be processed because it is not in a PENDING state',
@@ -275,7 +277,7 @@ export const processBulkUploadTask = async (
 
 	await updateBulkUploadTask(
 		db,
-		null,
+		taskAuthContext,
 		{
 			status: TaskStatus.IN_PROGRESS,
 		},
@@ -287,7 +289,7 @@ export const processBulkUploadTask = async (
 		graphileLogger,
 	).catch(async (err: unknown) => {
 		graphileLogger.warn('Download of bulk upload file from S3 failed', { err });
-		await createBulkUploadLog(db, taskRunnerAuthContext, {
+		await createBulkUploadLog(db, taskAuthContext, {
 			bulkUploadTaskId: bulkUploadTask.id,
 			isError: true,
 			details: getBulkUploadLogDetailsFromError(err),
@@ -297,7 +299,7 @@ export const processBulkUploadTask = async (
 	if (bulkUploadFile === undefined) {
 		await updateBulkUploadTask(
 			db,
-			null,
+			taskAuthContext,
 			{
 				status: TaskStatus.FAILED,
 			},
@@ -315,9 +317,11 @@ export const processBulkUploadTask = async (
 
 	try {
 		await assertBulkUploadTaskCsvIsValid(bulkUploadFile.path);
-		const opportunity =
-			await createOpportunityForBulkUploadTask(bulkUploadTask);
-		const applicationForm = await createApplicationForm(db, null, {
+		const opportunity = await createOpportunity(db, taskAuthContext, {
+			title: `Bulk Upload (${bulkUploadTask.createdAt})`,
+			funderShortCode: bulkUploadTask.funderShortCode,
+		});
+		const applicationForm = await createApplicationForm(db, taskAuthContext, {
 			opportunityId: opportunity.id,
 		});
 
@@ -334,30 +338,16 @@ export const processBulkUploadTask = async (
 		csvReadStream.pipe(parser);
 		let recordNumber = 0;
 
-		// This is a monkey patch to create an "auth context" for the sole purpose
-		// of populating `createdBy`.  This is shallow, and if we ever update our create
-		// queries to require a full auth context, this will not be sufficient.
-		const userAgentCreateAuthContext = {
-			user: await loadUserByKeycloakUserId(db, null, bulkUploadTask.createdBy),
-			role: {
-				isAdministrator: false,
-			},
-		};
-
 		await db.transaction(async (transactionDb) => {
 			await parser.forEach(async (record: string[]) => {
 				recordNumber += SINGLE_STEP;
-				const proposal = await createProposal(
-					transactionDb,
-					userAgentCreateAuthContext,
-					{
-						opportunityId: opportunity.id,
-						externalId: `${recordNumber}`,
-					},
-				);
+				const proposal = await createProposal(transactionDb, taskAuthContext, {
+					opportunityId: opportunity.id,
+					externalId: `${recordNumber}`,
+				});
 				const proposalVersion = await createProposalVersion(
 					transactionDb,
-					userAgentCreateAuthContext,
+					taskAuthContext,
 					{
 						proposalId: proposal.id,
 						applicationFormId: applicationForm.id,
@@ -372,7 +362,7 @@ export const processBulkUploadTask = async (
 				if (changemakerTaxId !== undefined) {
 					const changemaker = await createOrLoadChangemaker(
 						transactionDb,
-						taskRunnerAuthContext,
+						taskAuthContext,
 						{
 							name: changemakerName,
 							taxId: changemakerTaxId,
@@ -381,7 +371,7 @@ export const processBulkUploadTask = async (
 					);
 
 					if (changemaker !== undefined) {
-						await createChangemakerProposal(transactionDb, null, {
+						await createChangemakerProposal(transactionDb, taskAuthContext, {
 							changemakerId: changemaker.id,
 							proposalId: proposal.id,
 						});
@@ -400,21 +390,25 @@ export const processBulkUploadTask = async (
 							fieldValue,
 							applicationFormField.baseField.dataType,
 						);
-						return await createProposalFieldValue(transactionDb, null, {
-							proposalVersionId: proposalVersion.id,
-							applicationFormFieldId: applicationFormField.id,
-							value: fieldValue,
-							position: index,
-							isValid,
-							goodAsOf: null,
-						});
+						return await createProposalFieldValue(
+							transactionDb,
+							taskAuthContext,
+							{
+								proposalVersionId: proposalVersion.id,
+								applicationFormFieldId: applicationFormField.id,
+								value: fieldValue,
+								position: index,
+								isValid,
+								goodAsOf: null,
+							},
+						);
 					}),
 				);
 			});
 		});
 	} catch (err) {
 		graphileLogger.info('Bulk upload has failed', { err });
-		await createBulkUploadLog(db, taskRunnerAuthContext, {
+		await createBulkUploadLog(db, taskAuthContext, {
 			bulkUploadTaskId: bulkUploadTask.id,
 			isError: true,
 			details: getBulkUploadLogDetailsFromError(err),
@@ -427,7 +421,7 @@ export const processBulkUploadTask = async (
 	} catch (err) {
 		const message = `Cleanup of a temporary file failed (${bulkUploadFile.path})`;
 		graphileLogger.warn(message, { err });
-		await createBulkUploadLog(db, taskRunnerAuthContext, {
+		await createBulkUploadLog(db, taskAuthContext, {
 			bulkUploadTaskId: bulkUploadTask.id,
 			// `isError` is intended for UIs to find an explanation for bulk upload failure. Not this.
 			isError: false,
@@ -440,7 +434,7 @@ export const processBulkUploadTask = async (
 	if (bulkUploadHasFailed) {
 		await updateBulkUploadTask(
 			db,
-			null,
+			taskAuthContext,
 			{
 				status: TaskStatus.FAILED,
 			},
@@ -449,7 +443,7 @@ export const processBulkUploadTask = async (
 	} else {
 		await updateBulkUploadTask(
 			db,
-			null,
+			taskAuthContext,
 			{
 				status: TaskStatus.COMPLETED,
 			},

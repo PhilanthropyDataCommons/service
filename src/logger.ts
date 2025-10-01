205e1099f25e4458989481c7e49ebe6840aa401d
Jesse Bickel <jbickel@opentechstrategies.com>
2025-10-01 14:49:07 -0500

    Allow `undefined` to be sent to log censor
    With a pino upgrade, it appears an `undefined` is sent to the censor.
    This change more closely matches our censor function signature to that
    defined in pino's types. This way TypeScript can work for us instead
    of against us. In doing so, however, TSLint whines loudly, so we use
    `unknown` in place of `any` as we hope upstream does soon.
    
    Issue #1980 Bump pino from 9.11.0 to 9.12.0
    
    See also https://github.com/pinojs/pino/issues/2298 (proximate origin)
    and https://github.com/pinojs/pino/issues/2307 (related type issue)

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,37 @@
 import pino from 'pino';
 import type { Logger } from 'pino';
 
-// To prevent replays with JWT, we can redact the signature. This invalidates the JWT.
-export const redactToPreventAuthReplay = (secret: string): string =>
+/**
+ * To prevent replay attacks, redact the JWT signature. This invalidates it.
+ */
+const redactBearerString = (secret: string): string =>
 	secret.replace(/^(Bearer [A-Za-z0-9]*\.[A-Za-z0-9]*\.).*/, '$1[redacted]');
+
+/**
+ * This is a censor function adapter matching pino.redactOptions.censor.
+ * The scope of this function is far broader than our targeted use. We only
+ * do things when the path is what we expect.
+ * @see pino.redactOptions.censor
+ * @param value Pino has this as `any` but we adjusted it to `unknown`.
+ * @param string[] The object property path where the value was found.
+ */
+export const redactToPreventAuthReplay = (
+	value: unknown,
+	path: string[],
+): unknown => {
+	// The following line makes use of `path` to ensure we only act upon paths
+	// related to authorization. This means we throw an error in the narrow
+	// case that we care about (redacting authorization tokens) rather than any
+	// value that comes through our function from any context. While this check
+	// is redundant with the `paths` specified below in the pino options, it
+	// should make double-sure that we only throw an error in the expected case.
+	// Furthermore, making use of `path` means we don't have to silence tslint.
+	if (!path.includes('authorization') || value === undefined) return value;
+	if (typeof value === 'string') return redactBearerString(value);
+	throw new Error(
+		`Unexpected authorization value from pino. path: ${JSON.stringify(path)}, type: ${typeof value}, value: ${JSON.stringify(value)}`,
+	);
+};
 
 const logger = pino({
 	level: process.env.LOG_LEVEL ?? 'info',

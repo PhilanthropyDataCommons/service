81d1038684937244cb8669dde4863ce8f25c1961
Jesse Bickel <jbickel@opentechstrategies.com>
2025-10-21 10:39:15 -0500

    WIP abandoned this line of implementation
    Instead of #2063, favor #2065

--- src/types/BulkUploadTask.ts
+++ src/types/BulkUploadTask.ts
@@ -1,6 +1,7 @@
 import { ajv } from '../ajv';
 import { shortCodeSchema } from './ShortCode';
 import { idSchema } from './Id';
+import type { ApplicationForm } from './ApplicationForm';
 import type { BulkUploadLog } from './BulkUploadLog';
 import type { TaskStatus } from './TaskStatus';
 import type { JSONSchemaType } from 'ajv';
@@ -20,8 +21,10 @@ interface BulkUploadTask {
 	readonly proposalsDataFile: File;
 	attachmentsArchiveFileId: Id | null;
 	readonly attachmentsArchiveFile: File | null;
-	funderShortCode: ShortCode;
+	funderShortCode: ShortCode | null;
 	readonly funder: Funder;
+	applicationFormId: Id | null;
+	readonly applicationForm: ApplicationForm | null;
 	readonly status: TaskStatus;
 	readonly createdAt: string;
 	readonly createdBy: KeycloakId;
@@ -35,26 +38,69 @@ type InternallyWritableBulkUploadTask = WritableBulkUploadTask &
 
 const writableBulkUploadTaskSchema: JSONSchemaType<WritableBulkUploadTask> = {
 	type: 'object',
-	properties: {
-		sourceId: idSchema,
-		proposalsDataFileId: idSchema,
-		attachmentsArchiveFileId: {
-			...idSchema,
-			/* eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion --
-			 * This is a gross workaround for the fact that AJV does not support nullable types in TypeScript.
-			 * See: https://github.com/ajv-validator/ajv/issues/2163
-			 */
-			nullable: true as false,
+	required: [],
+	allOf: [
+		{
+			type: 'object',
+			properties: {
+				sourceId: idSchema,
+			},
+			required: ['sourceId'],
 		},
-		funderShortCode: {
-			...shortCodeSchema,
+		{
+			type: 'object',
+			properties: {
+				proposalsDataFileId: idSchema,
+			},
+			required: ['proposalsDataFileId'],
 		},
-	},
-	required: [
-		'sourceId',
-		'proposalsDataFileId',
-		'attachmentsArchiveFileId',
-		'funderShortCode',
+		{
+			type: 'object',
+			properties: {
+				attachmentsArchiveFileId: {
+					...idSchema,
+					/* eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion --
+					 * This is a gross workaround for the fact that AJV does not support nullable types in TypeScript.
+					 * See: https://github.com/ajv-validator/ajv/issues/2163
+					 */
+					nullable: true as false,
+				},
+			},
+			required: ['attachmentsArchiveFileId'],
+		},
+		{
+			type: 'object',
+			oneOf: [
+				{
+					type: 'object',
+					properties: {
+						funderShortCode: {
+							...shortCodeSchema,
+							/* eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion --
+							 * This is a gross workaround for the fact that AJV does not support nullable types in TypeScript.
+							 * See: https://github.com/ajv-validator/ajv/issues/2163
+							 */
+							nullable: true as false,
+						},
+					},
+					required: ['funderShortCode'],
+				},
+				{
+					type: 'object',
+					properties: {
+						applicationFormId: {
+							...idSchema,
+							/* eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion --
+							 * This is a gross workaround for the fact that AJV does not support nullable types in TypeScript.
+							 * See: https://github.com/ajv-validator/ajv/issues/2163
+							 */
+							nullable: true as false,
+						},
+					},
+					required: ['applicationFormId'],
+				},
+			],
+		},
 	],
 };
 

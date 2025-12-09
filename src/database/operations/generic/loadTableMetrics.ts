3993716c23c4fd1f228c86cbadc693b67c841439
Dan Schultz <slifty@gmail.com>
2025-12-09 17:28:27 -0500

    Explicitly coerce table metrics
    We are about to disable ajv coercion and so we want to explicitly coerce
    the string values returned by our table metrics query into the expected
    object types.
    
    Issue #2158 Disable ajv coercion

--- src/database/operations/generic/loadTableMetrics.ts
+++ src/database/operations/generic/loadTableMetrics.ts
@@ -3,23 +3,33 @@ import { isTableMetrics } from '../../../types';
 import { InternalValidationError } from '../../../errors';
 import type { TableMetrics } from '../../../types';
 
+interface RawTableMetrics {
+	count: string;
+	now: string;
+}
+
 export const loadTableMetrics = async (
 	tableName: string,
 ): Promise<TableMetrics> => {
-	const metricsQueryResult = await db.query<TableMetrics>(`
+	const metricsQueryResult = await db.query<RawTableMetrics>(`
     SELECT COUNT(*) AS "count",
       NOW() as "now"
     FROM ${tableName};
   `);
 
 	const {
-		rows: [metrics],
+		rows: [rawMetrics],
 	} = metricsQueryResult;
-	if (metrics === undefined) {
+	if (rawMetrics === undefined) {
 		throw new Error(
 			`Something went wrong collecting table metrics for ${tableName}`,
 		);
 	}
+
+	const metrics = {
+		count: Number(rawMetrics.count),
+		now: new Date(rawMetrics.now),
+	};
 
 	if (!isTableMetrics(metrics)) {
 		throw new InternalValidationError(
